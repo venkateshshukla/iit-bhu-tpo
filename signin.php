70d91b6ee98483d9a7448598d009ffc1394b8ddc
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-06-22 00:45:24 +0530

    Using google-api-php-client lib, authenticate user
    The implementation of signin.php is initiated. The idtoken sent to it by
    signin.js is verified and the userid of the user is acquired. This can be used
    by the backend to recognise the user.
    
    Added google-api-php-client as submodule.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- signin.php
+++ signin.php
@@ -1,3 +1,28 @@
 <?php
-echo 'Sample User'
 
+include_once('init.php');
+set_include_path(get_include_path() . PATH_SEPARATOR . EXROOT . 'dev/libraries/google-api-php-client/src');
+require_once('Google/autoload.php');
+
+$client_id = '244942183777-fi8bp76m3in1rueqjnkghp152d4hfpga.apps.googleusercontent.com';
+$client_secret = 'f6KsTE5nK_TKzE7HtqKeA3HY';
+$redirect_uri = 'signin.php';
+
+$client = new Google_Client();
+$client->setClientId($client_id);
+$client->setClientSecret($client_secret);
+$client->setRedirectUri($redirect_uri);
+$client->setScopes('email');
+
+function getUserFromToken($token, $client) {
+  $ticket = $client->verifyIdToken($token);
+  if ($ticket) {
+    $data = $ticket->getAttributes();
+    return $data['payload']['sub']; // user ID
+  }
+  return false;
+}
+
+$token = $_POST['idtoken'];
+echo getUserFromToken($token, $client);
+?>
