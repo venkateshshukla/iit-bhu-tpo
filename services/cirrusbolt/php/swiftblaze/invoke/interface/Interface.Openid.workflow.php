82212d1110e3b5c1139955bc8899ce34c0e28d35
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-06-22 23:24:48 +0530

    Moving towards integration of google authentication
    Now, the process of authentication as of yet is this -
    
    Google button etc are loaded with login.tile.html
    
    On sign-in the function onSignIn is called. This function extracts the idtoken
    from the response and redirects to auth.php with the idtoken included in the
    idtoken parameter.
    
    Auth.php calls the Interface.Openid.workflow.php with the idtoken sent inside
    the memory variable.
    
    A new run method has been added and the old one has been renamed to run_old. The
    new run loads the google-api-php-client, validates the idtoken and responds with
    all the details of the user including email, first and last name etc.
    
    Using this, do whatever the old script was doing.
    
    Unfortunately, I am still getting an AuthenticationError. Seems like the email
    id is not present in the response after validation.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- services/cirrusbolt/php/swiftblaze/invoke/interface/Interface.Openid.workflow.php
+++ services/cirrusbolt/php/swiftblaze/invoke/interface/Interface.Openid.workflow.php
@@ -18,10 +18,9 @@ require_once(LIGHTOPENID);
  *	@return continue string URL to continue [memory] optional default false
  *
  *	@author Vibhaj Rajan <vibhaj8@gmail.com>
- *	
 **/
 class InterfaceOpenidWorkflow implements Service {
-	
+
 	/**
 	 *	@interface Service
 	**/
@@ -30,16 +29,16 @@ class InterfaceOpenidWorkflow implements Service {
 			'optional' => array('openid_identifier' => false, 'continue' => false)
 		);
 	}
-	
+
 	/**
 	 *	@interface Service
 	**/
-	public function run($memory){
+	public function run_old($memory){
 		$session = Snowblozm::get('session');
 		$openid = new LightOpenID($_SERVER['HTTP_HOST']);
-		
+
 		try {
-			if(!$openid->mode && $memory['openid_identifier']){	
+			if(!$openid->mode && $memory['openid_identifier']){
 				$openid->identity = $memory['openid_identifier'];
 				$openid->required = array('contact/email', 'namePerson/first', 'namePerson/last');
 				header('Location: ' . $openid->authUrl());
@@ -54,7 +53,7 @@ class InterfaceOpenidWorkflow implements Service {
 			}
 			elseif($openid->validate()) {
 				$attr = $openid->getAttributes();
-				
+
 				if(!isset($attr['contact/email'])){
 					$memory['valid'] = false;
 					$memory['msg'] = 'Invalid OpenID';
@@ -62,9 +61,9 @@ class InterfaceOpenidWorkflow implements Service {
 					$memory['details'] = "OpenID Provider did not provide contact/email information @interface.openid";
 					return $memory;
 				}
-				
+
 				$memory['email'] = $attr['contact/email'];
-				
+
 				$workflow = array(
 				array(
 					'service' => 'guard.openid.find.workflow'
@@ -72,28 +71,28 @@ class InterfaceOpenidWorkflow implements Service {
 				array(
 					'service' => 'guard.key.info.workflow'
 				));
-				
+
 				$memory = Snowblozm::execute($workflow, $memory);
 				$register = false;
-				
+
 				if(!$memory['valid']){
 					$memory['valid'] = $register = true;
 					$session['expiry'] = 1;
 					$memory['user'] = $memory['email'];
 				}
-				
+
 				$workflow = array(
 				array(
 					'service' => 'cbcore.session.add.workflow',
 					'expiry' => $session['expiry'],
 					'output' => array('sessionid' => 'value')
 				));
-					
+
 				$memory = Snowblozm::execute($workflow, $memory);
-				
+
 				if(!$memory['valid'])
 					return $memory;
-				
+
 				//header('Location: '. isset($memory['continue']) ? $memory['continue'] : $session['root']);
 				setcookie($session['key'], $memory['value'], $register ? 0 : time()+60*60*24*$session['expiry'], '/');
 			}
@@ -112,25 +111,120 @@ class InterfaceOpenidWorkflow implements Service {
 			$memory['details'] = "Exception occurred : ".$e->getMessage()." @interface.openid";
 			return $memory;
 		}
-		
+
 		$memory['key'] = $session['key'];
 		$memory['expires'] = $session['expiry'];
 		$memory['continue'] = $memory['continue'] ? $memory['continue'] : $session['root'];
-		
+
 		$memory['valid'] = true;
 		$memory['msg'] = 'Valid Interface Session';
 		$memory['status'] = 200;
 		$memory['details'] = 'Successfully executed';
 		return $memory;
 	}
-	
+
+	/*
+	 * Run using the new openid connect
+	 */
+	public function run($memory){
+		$session = Snowblozm::get('session');
+
+		set_include_path(get_include_path() . PATH_SEPARATOR . EXROOT . 'dev/libraries/google-api-php-client/src');
+		require_once('Google/autoload.php');
+
+		$client_id = '244942183777-fi8bp76m3in1rueqjnkghp152d4hfpga.apps.googleusercontent.com';
+		$client_secret = 'f6KsTE5nK_TKzE7HtqKeA3HY';
+		$redirect_uri = '/auth.php';
+
+		$client = new Google_Client();
+		$client->setClientId($client_id);
+		$client->setClientSecret($client_secret);
+		$client->setRedirectUri($redirect_uri);
+		$client->setScopes('email');
+
+		$token = $memory['idtoken'];
+
+		try {
+			$ticket = $client->verifyIdToken($token);
+			if ($ticket) {
+				$data = $ticket->getAttributes();
+
+				if (!isset($data['email'])) {
+					$memory['valid'] = false;
+					$memory['msg'] = 'Invalid OpenID';
+					$memory['status'] = 500;
+					$memory['details'] = "OpenID Provider did not provide contact/email information @interface.openid";
+					return $memory;
+				}
+
+				$memory['email'] = $data['email'];
+
+				$workflow = array(
+				array(
+					'service' => 'guard.openid.find.workflow'
+				),
+				array(
+					'service' => 'guard.key.info.workflow'
+				));
+
+				$memory = Snowblozm::execute($workflow, $memory);
+				$register = false;
+
+				if(!$memory['valid']){
+					$memory['valid'] = $register = true;
+					$session['expiry'] = 1;
+					$memory['user'] = $memory['email'];
+				}
+
+				$workflow = array(
+					array(
+					'service' => 'cbcore.session.add.workflow',
+					'expiry' => $session['expiry'],
+					'output' => array('sessionid' => 'value')
+				));
+
+				$memory = Snowblozm::execute($workflow, $memory);
+
+				if(!$memory['valid'])
+					return $memory;
+
+				//header('Location: '. isset($memory['continue']) ? $memory['continue'] : $session['root']);
+				setcookie($session['key'], $memory['value'], $register ? 0 : time()+60*60*24*$session['expiry'], '/');
+
+			} else {
+				$memory['valid'] = false;
+				$memory['msg'] = 'Authentication Failed';
+				$memory['status'] = 407;
+				$memory['details'] = "User authentication failed @interface.openid";
+				return $memory;
+			}
+
+		} catch(ErrorException $e){
+			$memory['valid'] = false;
+			$memory['msg'] = 'Authentication Failure';
+			$memory['status'] = 407;
+			$memory['details'] = "Exception occurred : ".$e->getMessage()." @interface.openid";
+			return $memory;
+		}
+
+		$memory['key'] = $session['key'];
+		$memory['expires'] = $session['expiry'];
+		$memory['continue'] = $memory['continue'] ? $memory['continue'] : $session['root'];
+
+		$memory['valid'] = true;
+		$memory['msg'] = 'Valid Interface Session';
+		$memory['status'] = 200;
+		$memory['details'] = 'Successfully executed';
+		return $memory;
+	}
+
 	/**
 	 *	@interface Service
 	**/
 	public function output(){
 		return array('user', 'key', 'value', 'expires', 'continue');
 	}
-	
+
 }
 
 ?>
--- ui/js/signin.js
+++ ui/js/signin.js
@@ -1,7 +1,8 @@
 var signInInit = function() {
 	var values = {
 		client_id: '244942183777-fi8bp76m3in1rueqjnkghp152d4hfpga.apps.googleusercontent.com',
-		scope: 'openid email'
+		fetch_basic_profile: false,
+		scope: 'openid'
 	};
 
 	var onSignIn = function(googleUser) {
@@ -17,48 +18,23 @@ var signInInit = function() {
 
 	div = document.getElementById('googleButton');
 	auth2.attachClickHandler(element, {}, onSignIn, onError);
-
-};
-
-var sendToServer = function(url, idtoken) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', url);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onload = function() {
-		console.log('Signed in as: ' + xhr.responseText);
-	};
-	xhr.send('idtoken=' + idtoken);
 };
 
 var onSignIn = function() {
 	var auth2 = gapi.auth2.getAuthInstance();
-	if (auth2.isSignedIn.get()) {
-		document.getElementById('g-signout').style.visibility = "visible";
-		document.getElementById('g-disconnect').style.visibility = "visible";
-		var googleUser = auth2.currentUser.get();
-		var id = googleUser.getId();
-		var id_token = googleUser.getAuthResponse().id_token;
+	var googleUser = auth2.currentUser.get();
+	var id = googleUser.getId();
+	var id_token = googleUser.getAuthResponse().id_token;
 
-		var profile = googleUser.getBasicProfile();
-		var name = profile.getName();
-		var email = profile.getEmail();
-		console.log('ID: ' + id);
-		console.log('ID Token: ' + id_token);
-		console.log('Name: ' + name);
-		console.log('Email: ' + email);
+	console.log(id);
+	console.log(id_token);
 
-		sendToServer('signin.php', id_token);
-		console.log('Sent to server');
-	} else {
-		console.log("No user is signed in.")
-	}
+	window.location.replace('auth.php?idtoken=' + id_token);
 };
 
 var signOut = function() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	var callback = function() {
-		document.getElementById('g-signout').style.visibility = "hidden";
-		document.getElementById('g-disconnect').style.visibility = "hidden";
 		console.log("User signed Out.");
 	};
 	if (auth2.isSignedIn.get()) {
@@ -72,8 +48,6 @@ var revokeAll = function() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	if (auth2.isSignedIn.get()) {
 		auth2.disconnect();
-		document.getElementById('g-signout').style.visibility = "hidden";
-		document.getElementById('g-disconnect').style.visibility = "hidden";
 	} else {
 		console.log("No user is signed in.");
 	}
