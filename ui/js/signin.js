c11c4387b59753ddc0b128b4e5be2c4b53fb91d3
Ayush <ayush2913@gmail.com>
2015-06-24 02:48:35 +0530

    rectifying problems with customizinggoogle sign-in button

--- ui/js/signin.js
+++ ui/js/signin.js
@@ -1,21 +1,23 @@
 var googleUser = {};
 var signInInit = function() {
+	
 
 	gapi.load('auth2', function(){
       			// Retrieve the singleton for the GoogleAuth library and set up the client.
       			auth2 = gapi.auth2.init({
    				client_id: '244942183777-fi8bp76m3in1rueqjnkghp152d4hfpga.apps.googleusercontent.com',
-				cookiepolicy: 'single_host_origin',
+				//cookiepolicy: 'single_host_origin',
     				fetch_basic_profile: false,
     				scope: 'email'
   			});
       			auth2.attachClickHandler('customBtn', {}, onSuccess, onFailure);
 			
 			auth2.signIn().then(function() {
-				 
+				var auth2 = gapi.auth2.getAuthInstance(); 
 				var googleUser = auth2.currentUser.get();
 				var id = googleUser.getId();
 				var id_token = googleUser.getAuthResponse().id_token;
+				console.log('ayush');				
 				console.log(id);
 				console.log(id_token);
 				window.location.replace('auth.php?idtoken=' + id_token);
@@ -24,15 +26,23 @@ var signInInit = function() {
    		 });
 
 
-	var onSuccess = function(googleUser) {
-		console.log('Signed in as ' + user.getId());
-	};
 
-	var onFailure = function(error) {
-		console.log(error);
-		alert(JSON.stringify(error, undefined, 2));
-	};
+};
 
+var onSuccess = function(googleUser) {
+	var id = googleUser.getId();
+	var id_token = googleUser.getAuthResponse().id_token;
+	console.log('ayush');				
+	console.log(id);
+	console.log(id_token);
+	window.location.replace('auth.php?idtoken=' + id_token);
+
+	console.log('Signed in as ' + googleUser.getId());
+};
+
+var onFailure = function(error) {
+	console.log(error);
+	alert(JSON.stringify(error, undefined, 2));
 };
 
 
